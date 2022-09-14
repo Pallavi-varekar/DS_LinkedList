55d1edd263901f7d633dd003fa551efe90493595
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-09-14 13:47:43 +0530

    added:appending elements

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,6 @@
 public class LinkedList<T> {
     Node<T> head;
+    //uc1
     public void add(T data){
         Node<T> newNode =new Node<>();
         newNode.data = data;
@@ -15,6 +16,34 @@ public class LinkedList<T> {
             node.next=newNode;
         }
     }
+    //Uc3
+    public void addAtStart(T data) {
+        // head -> 56 30 70 -> null
+        // 100 null
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.next = head;
+
+        // 100 -> 30 40 50 -> null
+        head = newNode;
+        // head -> 100 -> 30 40 50 -> null
+    }
+    public void addAt(int index, T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+
+        if(index == 0) {
+            addAtStart(data);
+        } else {
+            // head -> 30 79 40 50 -> null
+            // index -> 2 data -> 79
+            Node<T> node = head;
+            for (int i = 0; i < index - 1; i++) {
+                node = node.next;
+            }
+            newNode.next = node.next;
+            node.next = newNode;
+        }}
     public void print(){
         Node<T> node = head;
         while (node.next != null){
@@ -23,4 +52,11 @@ public class LinkedList<T> {
         }
         System.out.println(node.data);
     }
+        public void print(int index) {
+            Node<T> node = head;
+            for (int i = 0; i < index - 1; i++) {
+                node = node.next;
+            }
+            System.out.println(node.data);
+        }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,13 @@
 public class Runner {
     public static void main(String[] args) {
-        LinkedList<Integer> integerLinkedList = new LinkedList<>();
-        integerLinkedList.add(56);
-        integerLinkedList.print();
-        integerLinkedList.add(30);
-        integerLinkedList.print();
-        integerLinkedList.add(70);
-        integerLinkedList.print();
 
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.addAt(0,56);
+        integerLinkedList.print();
+        integerLinkedList.addAt(1,30);
+        integerLinkedList.print();
+        integerLinkedList.addAt(2,70);
+        integerLinkedList.print();
     }
+
 }
